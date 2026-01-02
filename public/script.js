90d4e7a698d35467ffb34daaf11f7df93a4c681f
skuffazzer-ai <dadada1111111111111@gmail.com>
2026-01-02 16:06:20 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -8,7 +8,23 @@ let localStream;
 let peerConnection;
 let isCaller = false;
 
-const configuration = { iceServers: [{ urls: 'stun:stun.l.google.com:19302' }] };
+const configuration = {
+  iceServers: [
+    { urls: 'stun:stun.l.google.com:19302' },
+
+    {
+      urls: 'turn:openrelay.metered.ca:80',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    },
+    {
+      urls: 'turn:openrelay.metered.ca:443',
+      username: 'openrelayproject',
+      credential: 'openrelayproject'
+    }
+  ]
+};
+
 
 // Получаем локальный поток
 async function initLocalStream() {
